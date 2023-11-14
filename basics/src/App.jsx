be4fc096367c3f0ea04e4ed2193430f52f19e3cf
Ziarahman Musleh <ziarahmanmusleh@gmail.com>
2023-11-14 05:56:47 +0300

    day03

--- basics/src/App.jsx
+++ basics/src/App.jsx
@@ -4,11 +4,24 @@ import Jsx1 from "./components/02-jsx/Jsx1";
 import Jsx2 from "./components/02-jsx/Jsx2";
 import Jsx3 from "./components/02-jsx/Jsx3";
 import Jsx4 from "./components/02-jsx/Jsx4";
+import ExternalStyling from "./components/03-styles/ExternalStyling";
 import InlineStyling from "./components/03-styles/InlineStyling";
+import InternalStyling from "./components/03-styles/InternalStyling";
+import ModuleStyling from "./components/03-styles/ModuleStyling";
+import Scss from "./components/03-styles/Scss";
+import DigitalClock1 from "./components/04-digital-clock1/DigitalClock1";
 
 const App = () => {
     return (
         <div>
+            <div className="external-styling-container">
+                <h1>EXTERNAL STYLING</h1>
+                <ul id="external-styling-list">
+                    <li>Internal Styling</li>
+                    <li>Inline Styling</li>
+                    <li>External Styling</li>
+                </ul>
+            </div>
             <MyComponent />
             <AnotherComponent />
             {/* JSX */}
@@ -18,6 +31,12 @@ const App = () => {
             <Jsx4 />
             {/* STYLES */}
             <InlineStyling />
+            <InternalStyling />
+            <ExternalStyling />
+            <ModuleStyling />
+            <Scss />
+            {/* DIGITAL CLOCKS */}
+            <DigitalClock1 />
         </div>
     );
 };
